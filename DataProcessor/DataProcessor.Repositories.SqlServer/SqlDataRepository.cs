d82fa6396998b984e3042c206ceacfdeebbc7c27
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-06-16 06:19:26 -0400

    Added project for SqlDataRepository.

---
+++ DataProcessor/DataProcessor.Repositories.SqlServer/SqlDataRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataProcessor.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProcessor.Repositories.SqlServer
+{
+    public class SqlDataRepository : IDataRepository
+    {
+        public InitializeFileResult InitializeFile(InitializeFileRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertData(InsertRowRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertHeader(InsertRowRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTrailer(InsertRowRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFileStatusWithFileLoadedCompleted(SetFileStatusWithFileLoadedCompletedRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFileStatusWithWileLoadError(SetFileStatusWithFileLoadErrorRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
